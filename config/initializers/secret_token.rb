7f851e977c0db9af83129a20ed86c890194aebee
Andrey Deineko <andreydeineko@X-Teams-iMac.local>
2013-12-02 13:38:12 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodlist::Application.config.secret_key_base = '6df779eacd8da11e10ae2ae7d4bab87b0803ecdd4d56d065e5d1a70c653e5921a4d19ca66668bfda71b58dff71d56bd91698d2b8cd8aa8b94002e29a49b499af'
