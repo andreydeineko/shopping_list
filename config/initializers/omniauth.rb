56ccea63e348bc19156f731e599f04edb95938cf
Andrey Deineko <deineko.andrey@gmail.com>
2014-01-17 01:21:11 +0100

    new client-id and secret, but still no good

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
-#mniAuth.config.logger = Rails.logger
+OmniAuth.config.logger = Rails.logger
 
-#OmniAuth.config.full_host = "http://localhost:3000"
+OmniAuth.config.full_host = "http://localhost:3000"
 Rails.application.config.middleware.use OmniAuth::Builder do
   #:strategy_class => OmniAuth::Strategies::Google
-  provider :google_oauth2, '578631763686-sra9p3fkkci53q56sgrukff1624nlhu9.apps.googleusercontent.com', 'HNVpDTmtqUz478VtrO0hJD9M', provider_ignores_state: true, scope: "userinfo.email"
-  #{ :scope => 'https://www.googleapis.com/auth/email, https://www.googleapis.com/auth/userinfo.profile' }
+  provider :google_oauth2, '578631763686-1aorcmjr5ti62s0rtm01ath3ouhqa9qh.apps.googleusercontent.com', 'ffxhEyI8bAMIWxF84Bo-Vy3h', provider_ignores_state: true, scope: "userinfo.email"
+  { :scope => 'https://www.googleapis.com/auth/email, https://www.googleapis.com/auth/userinfo.profile' }
 end
