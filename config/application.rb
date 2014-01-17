9170311860612579f180529d8fc4fd4b565c7eaf
Andrey Deineko <deineko.andrey@gmail.com>
2014-01-17 14:01:01 +0100

    Adds heroku precompilation rules

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Foodlist
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w( active_admin.js application.css application.js  active_admin.css active_admin/print.css active_admin.js )
+
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Foodlist::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.assets.precompile += ['active_admin.js', 'active_admin.css', 'active_admin/print.css'] #for Heroku
+  config.assets.precompile += %w( active_admin.js application.css application.js  active_admin.css active_admin/print.css active_admin.js )
+  
 
 end
