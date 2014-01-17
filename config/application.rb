a0b242f4846a3629f8a60d7f6f0ed5d1d946cdda
Andrey Deineko <deineko.andrey@gmail.com>
2014-01-17 14:11:14 +0100

    modifies precompilation settings

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Foodlist
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w( active_admin.js application.css application.js  active_admin.css active_admin/print.css active_admin.js )
-
+    config.assets.initialize_on_precompile = false
   end
 end
