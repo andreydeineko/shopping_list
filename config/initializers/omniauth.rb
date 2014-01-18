79ed3cee627099ca8bf9b43dfa067bf676bd4cc3
Andrey Deineko <deineko.andrey@gmail.com>
2014-01-18 21:30:55 +0100

    changed config.full_host to http://shopplist.herokuapp.com

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 #OmniAuth.config.logger = Rails.logger
 
-OmniAuth.config.full_host = "http://localhost:3000" #"http://shopplist.herokuapp.com"
+OmniAuth.config.full_host = "http://shopplist.herokuapp.com"
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '578631763686-t5ucjlaj2d5um54rq33qgrs76i4kl98n.apps.googleusercontent.com', 
   'p-ARY93GFDoOIsSTlsn4sPPo', scope: 'userinfo.email, userinfo.profile', provider_ignores_state: true
