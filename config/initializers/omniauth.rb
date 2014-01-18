42cafd31520e98c89265b215abe8a6b382d92e9a
Andrey Deineko <deineko.andrey@gmail.com>
2014-01-18 21:09:41 +0100

    facebook authentication added

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 #OmniAuth.config.logger = Rails.logger
 
-OmniAuth.config.full_host = "http://shopplist.herokuapp.com"
+OmniAuth.config.full_host = "http://localhost:3000" #"http://shopplist.herokuapp.com"
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #:strategy_class => OmniAuth::Strategies::Google
-  provider :google_oauth2, '578631763686-t5ucjlaj2d5um54rq33qgrs76i4kl98n.apps.googleusercontent.com', 'p-ARY93GFDoOIsSTlsn4sPPo', provider_ignores_state: true, scope: "userinfo.email"
+  provider :google_oauth2, '578631763686-t5ucjlaj2d5um54rq33qgrs76i4kl98n.apps.googleusercontent.com', 
+  'p-ARY93GFDoOIsSTlsn4sPPo', scope: 'userinfo.email, userinfo.profile', provider_ignores_state: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117120333) do
+ActiveRecord::Schema.define(version: 20140118181331) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -103,6 +103,8 @@ ActiveRecord::Schema.define(version: 20140117120333) do
     t.integer  "spam_comcoms_count",          default: 0
     t.string   "name"
     t.boolean  "admin",                       default: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
