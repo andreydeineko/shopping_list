40a46fcc914efb6c1c0149a335aa2d6d78ce6b46
Andrey Deineko <deineko.andrey@gmail.com>
2014-02-04 15:17:54 +0100

    fixed sorting problems. moved voting to the left. added saveSort and savePagerSize functions.

--- app/assets/javascripts/authenticated/items.js
+++ app/assets/javascripts/authenticated/items.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 
-    // $('#items th.header').on('mouseover', function(event) {
-    //     $(this).('th.header').animate({'margin-top': '-10px'}, 350);
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    // });
+    $("#items")
+        .tablesorter({widthFixed: true, widgets: ['zebra', 'saveSort', 'savePagerSize']}) 
+        .tablesorterPager({container: $("#pager"), savePages: true }); 
 
-    // $("tr[data-link]").click(function() {
-    //     window.location = this.dataset.link
-    // });
+//  function to save table's sorted state 
+    $('button').click(function(event){
+        $('#items')
+            .trigger('saveSortReset') // clear saved sort
+            .trigger('sortReset');    // reset current table sort
+        return true;
+    });
 
-    $("#items").tablesorter({widthFixed: true, widgets: ['zebra']}) 
-    .tablesorterPager({container: $("#pager")}); 
 
     $('a#new-item-container').click(function(event){
         $('#new-item').fadeToggle();
@@ -20,16 +20,17 @@ $(document).ready(function() {
     });
 
 //  abbr[title] was "required", but we need it to be "Required"
-    function capitaliseFirstLetter(string){
+    function capitaliseFirstLetter(string)
+        {
         return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
+        }
     $('.simple_form .required abbr').attr('title', capitaliseFirstLetter($('.simple_form .required abbr').attr('title')));
 
     $('.simple_form .required abbr').hover(
         function(){
-        $(this).append($("<span>This field shouldn't be blank</span>"));
-    }, function() {
-        $(this).find("span:last").remove();
+            $(this).append($("<span>This field shouldn't be blank</span>"));
+    },  function() {
+            $(this).find("span:last").remove();
     });
+
 });
