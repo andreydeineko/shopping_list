a143a114254f3e6eec8beba8d42618c2ee17cf7e
Andrey Deineko <deineko.andrey@gmail.com>
2014-06-18 11:10:48 +0200

    changed content type from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206115856) do
+ActiveRecord::Schema.define(version: 20140618090809) do
 
   create_table "comments", force: true do |t|
-    t.string   "content"
+    t.text     "content",    limit: 255
     t.integer  "item_id"
     t.integer  "user_id"
     t.datetime "created_at"
